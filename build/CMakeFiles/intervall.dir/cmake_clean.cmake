0e1a79cb0d37f23fe66696da971ee67c63e000d3
mamadou.Dembele <mamadou.dembele@ensta-bretagne.org>
2020-02-27 08:38:42 +0100

    premiere version du dossier build inutile ici

---
+++ build/CMakeFiles/intervall.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/intervall.dir/src/intervall.cpp.o"
+  "libintervall.pdb"
+  "libintervall.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/intervall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
